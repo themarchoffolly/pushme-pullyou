f044034a92f02a1b26fa87a672af19c02d6711a1
David Russell <themarchoffolly@gmail.com>
2017-06-07 17:27:57 +0700

    Test code delimiter lang hint option.

--- PITCHME.md
+++ PITCHME.md
@@ -107,3 +107,17 @@ Next slide is implicit about php highlight type
 
 @[1]
 @[3-5]
+
+---
+
+Next slide uses code delimiter no lang hint
+
+---?code=src/header.php
+
+---
+
+Next slide uses code delimiter with lang hint php
+
+---?code=src/header.php
+
+
---
+++ src/header.php
@@ -0,0 +1,18 @@
+// <![CDATA[
+<html>
+<header>
+  <script	
+    src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">
+  </script>
+  <script 
+    src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js">
+  </script>
+  <link rel="stylesheet" type="text/css" 
+    href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css">
+  </link>
+  
+  <link rel="stylesheet" type="text/css" href="demo.css"/>
+  <script type="text/javascript" src="demo.js"></script>
+</header>
+<body>
+// ]]>
